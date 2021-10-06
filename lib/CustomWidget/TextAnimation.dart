6892e4924aa371cac550b12e8d27e76d36d67a1e
Unseen <harry93290@gmail.com>
2021-10-06 18:00:29 +0200

    [ERREUR] I have probleme with TextAnimation.dart , it must be modified

---
+++ lib/CustomWidget/TextAnimation.dart
@@ -0,0 +1,48 @@
+/*import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'RegisterButton.dart';
+import '../Page/Register.dart';
+import '../Page/Authentification.dart';
+
+class TextAnimation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 50),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Hero(
+                        tag: "DoobleLogo",
+                        child: Container(
+                          child: Image.asset("Assets/Images/DoobleLogo.png"),
+                          height: 90,
+                        ),
+                      ),
+
+                        ],
+                      ),
+
+                  ),
+                ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+// Creation du mot en defilement dans le debut de l'application
+*/
